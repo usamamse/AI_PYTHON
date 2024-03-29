b5ce7af431aa836be61fe08cc4c530763029c3bb
usamamse <147350238+usamamse@users.noreply.github.com>
2024-03-29 01:58:10 +0100

    Delete exercice3.py

--- exercice3.py
+++
@@ -1,8 +0,0 @@
-import math 
-
-def conversionTemps(secondes):
-    heures = math.floor(secondes/3600)
-    minutes = heures %60
-    secondes = secondes %60
-    return str(heures)+" heures "+str(minutes)+" minutes "+str(secondes)+" secondes"
-print(conversionTemps(56263))
